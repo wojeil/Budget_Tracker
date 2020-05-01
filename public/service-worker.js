32e82a849e24bdb59ca9fdc6b17af728a6f30475
Wadih Ojeil <wadih.ojeil@gmail.com>
2020-04-30 21:29:34 -0400

    added new files to cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const FILES_TO_CACHE = ["/", "/index.html", "index.js", "style.css"];
+const FILES_TO_CACHE = ["/", "/index.html", "index.js", "style.css","icons/icon-192x192.png","icons/icon-512x512.png"];
 
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
