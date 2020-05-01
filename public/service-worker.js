44fbc713c6dc9b4161b4d2906668b912d7ee1e6d
Wadih Ojeil <wadih.ojeil@gmail.com>
2020-05-01 15:10:26 -0400

    heroku dep

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const FILES_TO_CACHE = ["/", "/index.html", "index.js", "style.css","icons/icon-192x192.png","icons/icon-512x512.png"];
+const FILES_TO_CACHE = ["/", "/index.html", "index.js", "styles.css","db.js","icons/icon-192x192.png","icons/icon-512x512.png"];
 
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
@@ -35,7 +35,7 @@ self.addEventListener("activate", function(evt) {
 
 // fetch
 self.addEventListener("fetch", function(evt) {
-    if (evt.request.url.includes("/")) {
+    if (evt.request.url.includes("/api/")) {
       evt.respondWith(
         caches.open(DATA_CACHE_NAME).then(cache => {
           return fetch(evt.request)
