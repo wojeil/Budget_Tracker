928a204453c0ec50cf773c7a8c1061227c8f7b08
Wadih Ojeil <wadih.ojeil@gmail.com>
2020-06-05 11:01:22 -0400

    added bootstrap buttons and background

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const FILES_TO_CACHE = ["/", "/index.html", "index.js", "styles.css","db.js","icons/icon-192x192.png","icons/icon-512x512.png"];
+const FILES_TO_CACHE = ["/", "/index.html", "index.js", "styles.css","db.js","icons/icon-192x192.png","icons/icon-512x512.png, image/crossline-lines.png"];
 
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
